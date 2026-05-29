b590564f826af0a06844572199dec9f0023507b4
Larissa Wieck <rm98826@estudante.fieb.edu.br>
2026-05-29 11:55:51 -0300

    correção

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
  
 router.get("/", (req, res)=>{
-    res.render("pages/index",{"retorno":null,"valores":{"dia":""}});
+    res.render("index",{"retorno":null,"valores":{"dia":""}});
 });
  
  
